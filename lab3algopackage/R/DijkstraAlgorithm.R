4924c1b56d248e100745fc7f4316db2cdfbf7137
mahmood siddique <mahmood1s@gmail.com>
2019-09-15 10:53:59 +0200

    error fixed in examples

--- lab3algopackage/R/DijkstraAlgorithm.R
+++ lab3algopackage/R/DijkstraAlgorithm.R
@@ -6,16 +6,20 @@
 #' @param init_node A number as starting node.
 #' @return The shortest path vector from \code{init_node} and all other nodes.
 #' @examples
+#' v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6)
+#' v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5)
+#' w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)
+#' wiki_graph <-data.frame(v1,v2,w)
 #' dijkstra(wiki_graph, 1)
 #' dijkstra(wiki_graph, 3)
 
-dijkstra<- function(df,init_node){
+dijkstra<- function(my_df,init_node){
 
   #get the number of nodes in graph
-  node_range<- unique(df[,1],incomparables = FALSE)
+  node_range<- unique(my_df[,1],incomparables = FALSE)
 
   #converting data.frame to matrix
-  my_matrix<-convert_to_matrix(df,node_range)
+  my_matrix<-convert_to_matrix(my_df,node_range)
 
   #stopif init_node is not with graph range
   stopifnot(init_node %in% node_range)
@@ -62,7 +66,7 @@ minimum_path_function<- function(distance_vector,visited){
   return(min_index)
 }
 
-convert_to_matrix<- function(df,node_range){
+convert_to_matrix<- function(my_df,node_range){
 
   my_matrix<-matrix(c(0:0),ncol=length(node_range),nrow = length(node_range),byrow = TRUE)
   my_row<-1
@@ -71,9 +75,9 @@ convert_to_matrix<- function(df,node_range){
   {
     for(j in 1:length(node_range))
     {
-      if(df[my_row,1]==i & my_row<=length(df[,1]))
+      if(my_df[my_row,1]==i & my_row<=length(my_df[,1]))
       {
-        my_matrix[df[my_row,1],df[my_row,2]]<-df[my_row,3]
+        my_matrix[my_df[my_row,1],my_df[my_row,2]]<-my_df[my_row,3]
         my_row<-my_row+1
       }
     }
