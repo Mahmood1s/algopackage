7b12f9f7a79e2bbb07bd9b32ab1365d4b2bce1ce
rakhshanda75 <55328437+rakhshanda75@users.noreply.github.com>
2019-09-18 21:41:52 +0200

    dataset for wiki_graph

---
+++ dataset_info.R
@@ -0,0 +1,15 @@
+
+#'#' wiki_graph for analysis
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format @format  A dataframe with 3 variables
+#' \describe{
+#' @\item {v1}{set of vertices}
+#' @\item {v2}{set of vertices}
+#' @\item {w}{sei of edge weigths}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+
+ "wiki_graph"
